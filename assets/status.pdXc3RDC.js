c59d93df2afba2fa49858be64bfd9d65456e4295
AlexandrStarDev <soalexandr0303@gmail.com>
2025-10-20 16:56:58 +0100

    fixed

---
+++ assets/status.pdXc3RDC.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as e,l as a,g as p}from"./index-37Su0qRO.js";const r=s({props:{type:{type:String,default:"primary"},pulse:{type:[Boolean,Number],default:!0}}},[["render",function(s,r,o,c,n,l){const u=p;return t(),e(u,{class:a(["sc-state",[{"sc-status-processing":o.pulse},"sc-state-bg--"+o.type]])},null,8,["class"])}],["__scopeId","data-v-f65265b9"]]);export{r as s};
